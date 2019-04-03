a70b4439b3ff9f2c642975e59166c8236d94aeb3
jalvarezotero <you@example.com>
2019-04-03 12:02:02 +0200

    Jframe creado

---
+++ src/boletin_25/Boletin_25.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_25;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+public class Boletin_25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventana1 vent1 = new Ventana1();
+        vent1.setVisible(true);
+        
+    }
+    
+}
